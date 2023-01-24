23f15852294ecffeaf6d7d8b398077e1fd9d6899
DevCosmo <silentcosmox@gmail.com>
2023-01-24 22:02:08 +0530

    v0.2.0

--- src/App.js
+++ src/App.js
@@ -24,149 +24,159 @@ function App() {
       </div>
 
       <div className="container">
-        <div className="col-md-4">
-          <div className="todos">
-            <div className='dph'></div>
+        <div className="row">
+          {/* success */}
+          <div className="col-md-4">
+            <div className="todos">
+              
 
-            {
-              toDos.map((obj2) => {
-                if (obj2.status) {
-                  return (
-                    <div className="todo-done">
-                      <h1 className='ttlr'>{obj2.text}</h1>
-                      <p className='time'>Completed Time: {moment(obj2.finish).format("HH:mm")}</p>
-                    </div>
-                  )
-                }
-                return null
-              })
-            }
-
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div className="todos">
-            <div className='dph'></div>
-            {
-              toDos.map((obj) => {
-
-                if (obj.faild === false && obj.status === false) {
-                  return (
-                    <div className="todo">
-                      <input type="checkbox" onChange={(e) => {
-
-                        setToDos(toDos.filter(obj2 => {
-                          if (obj2.id === obj.id) {
-                            obj2.status = e.target.checked
-                            obj2.finish = Date()
-                          }
-                          return obj2
-                        }))
-                      }} />
-
-
-                      <h3 className="task-title"> {obj.text}</h3>
-
-                      <h2 className="remove" onClick={() => {
-                        setToDos(toDos.filter(obj3 => {
-
-                          if (obj3.id === obj.id && obj3.text !== "silentcosmo" && obj3.text !== "SilentCosmo" && obj3.text !== "SilentCosmo" && obj3.text !== "silentCosmo") {
-
-
-                            obj3.faild = true
-
-                          }
-                          if (obj3.text === "silentcosmo") {
-                            obj3.status = true
-                          }
-                          console.log("new: ", obj3);
-                          return obj3
-                        }))
-                      }}>x</h2>
-
-                      <p className='time-main'>Time {moment(obj.time).format("HH:mm")}</p>
-                    </div>
-                  )
-                } else {
-                  /* If todo status changed */
-                  return (
-                    <div className="todo">
-                      <input type="checkbox" onChange={(e) => {
-
-                        setToDos(toDos.filter(obj2 => {
-                          if (obj2.id === obj.id && obj.faild === false) {
-                            obj2.status = e.target.checked
-                            obj2.finish = Date()
-                          }
-                          return obj2
-                        }))
-                      }} />
-
-
-                      <h3 className="task-title remove-title"> {obj.text}</h3>
-
-                      <h2 className="remove" onClick={() => {
-                        setToDos(toDos.filter(obj3 => {
-
-                          if (obj3.id === obj.id && obj3.status === false) {
-
-                            obj3.faild = true
-                          }
-                          console.log("new: ", obj3);
-                          return obj3
-                        }))
-                      }}>x</h2>
-
-                      <p className='time-main'>Time {moment(obj.time).format("HH:mm")} {/* Date {moment(obj.time).format("DD:MM:YYYY") */}</p>
-                    </div>
-                  )
-                }
-
-              })
-
-            }
-          </div>
-
-        </div>
-
-        <div className="col-md-4">
-          <div className="todos">
-
-            <div>
               {
-                toDos.map((obj3) => {
-                  if (obj3.faild) {
+                toDos.map((obj2) => {
+                  if (obj2.status) {
                     return (
-                      <div className="todo-fail">
-                        <h1 className='ttlr'>{obj3.text}</h1>
-
-                        <h2 className="redo" onClick={() => {
-                          setToDos(toDos.filter(faild => {
-
-                            if (faild.id === obj3.id) {
-                              faild.faild = false
-                            }
-                            console.log("new: ", obj3);
-                            return faild
-                          }))
-                        }}>↩️</h2>
-
-                        <p className='time'>Failed Time: {moment(obj3.finish).format("HH:mm")}</p>
+                      <div className="todo-done mx-auto">
+                        <h1 className='ttlr'>{obj2.text}</h1>
+                        <p className='time'>Completed Time: {moment(obj2.finish).format("HH:mm")}</p>
                       </div>
                     )
                   }
                   return null
                 })
               }
+
+            </div>
+          </div>
+          {/* New todo */}
+          <div className="col-md-4">
+            <div className="todos mx-auto">
+              {/* <div className='dph'></div> */}
+              {
+                toDos.map((obj) => {
+
+                  if (obj.faild === false && obj.status === false) {
+                    return (
+                      <div className="todo mx-auto">
+                        <input type="checkbox" onChange={(e) => {
+
+                          setToDos(toDos.filter(obj2 => {
+                            if (obj2.id === obj.id) {
+                              obj2.status = e.target.checked
+                              obj2.finish = Date()
+                            }
+                            return obj2
+                          }))
+                        }} />
+
+
+                        <h3 className="task-title"> {obj.text}</h3>
+
+                        <h2 className="remove" onClick={() => {
+                          setToDos(toDos.filter(obj3 => {
+
+                            if (obj3.id === obj.id && obj3.text !== "silentcosmo" && obj3.text !== "SilentCosmo" && obj3.text !== "SilentCosmo" && obj3.text !== "silentCosmo") {
+
+
+                              obj3.faild = true
+
+                            }
+                            if (obj3.text === "silentcosmo") {
+                              obj3.status = true
+                            }
+                            console.log("new: ", obj3);
+                            return obj3
+                          }))
+                        }}>
+                           <i className="fa-solid fa-xmark"></i>
+                        </h2>
+
+                        <p className='time-main'>Time {moment(obj.time).format("HH:mm")}</p>
+                      </div>
+                    )
+                  } else {
+                    /* If todo status true */
+                    return (
+                      <div className="todo mx-auto">
+                        <input type="checkbox" onChange={(e) => {
+
+                          setToDos(toDos.filter(obj2 => {
+                            if (obj2.id === obj.id && obj.faild === false) {
+                              obj2.status = e.target.checked
+                              obj2.finish = Date()
+                            }
+                            return obj2
+                          }))
+                        }} />
+
+
+                        <h3 className="task-title remove-title"> {obj.text}</h3>
+
+                        <h2 className="remove-false" onClick={() => {
+                          setToDos(toDos.filter(obj3 => {
+
+                            if (obj3.id === obj.id && obj3.status === false) {
+
+                              obj3.faild = true
+                            }
+                            console.log("new: ", obj3);
+                            return obj3
+                          }))
+                        }}>
+                          <i className="fa-regular fa-circle-check"></i>
+                        </h2>
+
+                        <p className='time-main'>Time {moment(obj.time).format("HH:mm")} {/* Date {moment(obj.time).format("DD:MM:YYYY") */}</p>
+                      </div>
+                    )
+                  }
+
+                })
+
+              }
             </div>
 
           </div>
+          {/* Failed */}
+          <div className="col-md-4">
+            <div className="todos">
+
+              <div>
+                {
+                  toDos.map((obj3) => {
+                    if (obj3.faild) {
+                      return (
+                        <div className="todo-fail mx-auto">
+                          <h1 className='ttlr'>{obj3.text}</h1>
+
+                          <h2 className="redo" onClick={() => {
+                            setToDos(toDos.filter(faild => {
+                              console.log(obj3);
+                              if (faild.id === obj3.id) {
+                                faild.faild = false
+                              }
+                              console.log("new: ", obj3);
+                              return faild
+                            }))
+                          }}>
+                            <i className="fa-solid fa-arrow-rotate-left"></i>
+                          </h2>
+
+                          <p className='time'>Failed Time: {moment(obj3.finish).format("HH:mm")}</p>
+                        </div>
+                      )
+                    }
+                    return null
+                  })
+                }
+              </div>
+
+            </div>
 
 
+          </div>
+        </div>
         </div>
       </div>
-    </div>
+    
   );
 }
 
